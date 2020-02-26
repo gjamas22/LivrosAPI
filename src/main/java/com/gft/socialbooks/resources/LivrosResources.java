66e0b4b275e36ce172768efe005e20f2be539ecb
Gabrielli Jamas <gjamas22@gmail.com>
2020-02-26 14:59:23 -0300

    Primeiro update

---
+++ src/main/java/com/gft/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,29 @@
+package com.gft.socialbooks.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gft.socialbooks.domain.Livro;
+
+@RestController
+public class LivrosResources {
+
+	
+	@RequestMapping( value = "/livros", method = RequestMethod.GET)
+	public List<Livro> listar() {
+		
+		Livro l1 = new Livro("Rest Aplicado");
+		Livro l2 = new Livro("Git passo-a-passo");
+	
+		Livro[] livros = {l1,l2};
+		
+		return Arrays.asList(livros);
+	}
+	
+	
+	
+}
