678191ff2051cdffcd180c2e0d0acc39aeeee607
Gabrielli Jamas <gjamas22@gmail.com>
2020-02-27 14:49:01 -0300

    Segundo Update

--- src/main/java/com/gft/socialbooks/domain/Livro.java
+++ src/main/java/com/gft/socialbooks/domain/Livro.java
@@ -3,12 +3,20 @@ package com.gft.socialbooks.domain;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@Entity
 public class Livro {
 
 	@JsonInclude(Include.NON_NULL)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
@@ -23,6 +31,7 @@ public class Livro {
 	private String resumo;
 	
 	@JsonInclude(Include.NON_NULL)
+	@OneToMany(mappedBy = "livro")
 	private List<Comentario> comentarios;
 	
 	@JsonInclude(Include.NON_NULL)
